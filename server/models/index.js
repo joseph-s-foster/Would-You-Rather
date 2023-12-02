61b0e0592ffaa5dba3eba27bb9976e94444c2ebe
Lucas Jerome Lovelace <lucasjeromelovelace@Lucass-Air.attlocal.net>
2023-12-02 12:59:06 -0600

    updated server models

--- server/models/index.js
+++ server/models/index.js
@@ -1,5 +1,4 @@
 const User = require('./User');
-const Thought = require('./Thought');
 const Polls = require('./Polls')
 
-module.exports = { User, Thought, Polls };
+module.exports = { User, Polls };
