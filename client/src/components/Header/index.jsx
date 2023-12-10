f5df2ddb500f4c9f41d03fcf8372d20e7ff52e2d
D3C0D1NG <joseph.s.foster@icloud.com>
2023-12-09 21:07:29 -0600

    improved page nav ux

--- client/src/components/Header/index.jsx
+++ client/src/components/Header/index.jsx
@@ -1,37 +1,33 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import Auth from "../../utils/auth";
 
 const Header = () => {
+  const location = useLocation();
   const logout = (event) => {
     event.preventDefault();
     Auth.logout();
     window.location.href = "/"; // Redirect to the home page
   };
-  
 
   return (
     <header className="bg-primary text-light mb-4 flex-row align-center">
       <div className="container flex-row justify-space-between-lg justify-center align-center">
         <div>
           <Link className="text-light" to="/">
-            <h1 className="mt-2">Would You Rather</h1>
+            <h1 className="mb-2 mt-1">Would You Rather</h1>
           </Link>
           {Auth.loggedIn() ? (
-            <>
             <p></p>
-            </>
           ) : (
-            <>
-          <p className="m-1">
-            Login to create polls and submit data.
-          </p>
-          </>
+            location.pathname !== "/login" && (
+              <p className="mb-2">Login to create polls and submit data.</p>
+            )
           )}
         </div>
         <div>
           {Auth.loggedIn() ? (
             <>
-             <Link className="btn btn-lg btn-danger m-2" to="/user-polls">
+              <Link className="btn btn-lg btn-danger m-2" to="/user-polls">
                 User Polls
               </Link>
               <button className="btn btn-lg btn-light ml-2" onClick={logout}>
@@ -39,11 +35,12 @@ const Header = () => {
               </button>
             </>
           ) : (
-            <>
+            // Render the "Login" button only if not on the "/login" page
+            location.pathname !== "/login" && (
               <Link className="btn btn-lg btn-info m-2" to="/login">
                 Login
               </Link>
-            </>
+            )
           )}
         </div>
       </div>
