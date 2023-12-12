68b77e0aa5ded0725ab00d9a39af5139275907e3
D3C0D1NG <joseph.s.foster@icloud.com>
2023-12-11 19:49:38 -0600

    heroku db setup

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/wyr-db');
+// dev
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/wyr-db');
 
+// prod
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/wyr');
 module.exports = mongoose.connection;
