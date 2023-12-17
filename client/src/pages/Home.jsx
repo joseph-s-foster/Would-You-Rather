fcd1ba5c6adfd24b3800653076025b813cf68024
D3C0D1NG <joseph.s.foster@icloud.com>
2023-12-17 14:18:14 -0600

    styled pagination for mobile view

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,13 +1,31 @@
 import { useQuery } from "@apollo/client";
 import PollCard from "../components/PollCard";
 import { GET_POLLS_QUERY } from "../utils/queries";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const Home = () => {
   const { loading, data } = useQuery(GET_POLLS_QUERY, { pollInterval: 1000 });
   const polls = data?.getPolls || [];
   const [currPage, setCurrPage] = useState(1);
-  const itemsPerPage = 9;
+  const [itemsPerPage, setItemsPerPage] = useState(9); // Default items per page
+
+  useEffect(() => {
+    // Check for mobile view and adjust itemsPerPage
+    const handleResize = () => {
+      setItemsPerPage(window.innerWidth <= 768 ? 3 : 9);
+    };
+
+    handleResize(); // Set initial value
+
+    // Update itemsPerPage when the window is resized
+    window.addEventListener("resize", handleResize);
+
+    // Cleanup event listener on component unmount
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []); // Empty dependency array to run the effect only once on mount
+
   const startIndex = (currPage - 1) * itemsPerPage;
   const endIndex = startIndex + itemsPerPage;
   const currItems = polls.slice(startIndex, endIndex);
@@ -35,7 +53,7 @@ const Home = () => {
           onClick={() => handlePageChange(currPage - 1)}
           disabled={currPage === 1}
           className="btn btn-primary mr-1"
-          style={{width:"95px"}}
+          style={{ width: "95px" }}
         >
           Prev
         </button>
@@ -43,7 +61,7 @@ const Home = () => {
           onClick={() => handlePageChange(currPage + 1)}
           disabled={currPage === totalPages}
           className="btn btn-primary ml-1"
-          style={{width:"96px"}}
+          style={{ width: "96px" }}
         >
           Next
         </button>
