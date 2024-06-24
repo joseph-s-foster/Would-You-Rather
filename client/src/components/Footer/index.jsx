601f7707bdbbea5dec4748cdaf8cf95c535d7af4
D3C0D1NG <joseph.s.foster@icloud.com>
2024-06-24 18:26:31 -0500

    added footer icons

--- client/src/components/Footer/index.jsx
+++ client/src/components/Footer/index.jsx
@@ -1,4 +1,5 @@
 import { useLocation, useNavigate } from "react-router-dom";
+import { ArrowUpCircleIcon, HomeModernIcon } from "@heroicons/react/24/outline";
 
 const Footer = () => {
   const location = useLocation();
@@ -18,18 +19,51 @@ const Footer = () => {
           </span>{" "}
           by Group1
         </h4> */}
-        <a
-          href="https://github.com/joseph-s-foster/Would-You-Rather"
-          target="_blank"
-          rel="noopener noreferrer"
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            padding: "48px 0 48px 0",
+          }}
         >
-          <img
-            src="./github.png"
-            alt="github logo"
-            style={{ margin: "48px", height: "40px", width: "40px" }}
-          />
-        </a>
-        <p style={{ color: "#d9e9e8"}}>Copyright &copy; 2024 Would You Rather</p>
+          <a style={{ cursor: "pointer" }}>
+            <HomeModernIcon
+              style={{
+                margin: "2px 0 0 0",
+                color: "fff",
+                height: "48px",
+                width: "48px",
+              }}
+              aria-hidden="true"
+            />
+          </a>
+          <a style={{ cursor: "pointer" }}>
+            <ArrowUpCircleIcon
+              style={{
+                margin: "2px 48px 0 48px",
+                color: "fff",
+                height: "54px",
+                width: "54px",
+              }}
+              aria-hidden="true"
+            />
+          </a>
+          <a
+            href="https://github.com/joseph-s-foster/Would-You-Rather"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              src="./github.png"
+              alt="github logo"
+              style={{ margin: "2px 5px 0 5px", height: "43px", width: "43px" }}
+            />
+          </a>
+        </div>
+        <p style={{ marginBottom: "48px", color: "#d9e9e8" }}>
+          Copyright &copy; 2024 Would You Rather
+        </p>
       </div>
     </footer>
   );
